37a5bd2c732fd017ac535c92649cffd97e9a3183
angela1011 <angela.891011@gmail.com>
2024-05-16 13:20:54 +0800

    update commit

---
+++ TimeSerise/run.sh
@@ -0,0 +1,9 @@
+git add .
+git config user.name angela1011
+git config user.email angela.891011@gmail.com
+git commit -m 'update commit'
+git push
+
+#git pull
+#git config pull.rebase false  # merge
+#git config pull.rebase true # rebase
